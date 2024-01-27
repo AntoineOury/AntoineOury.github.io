51222bb7cebfee81dd11faf96151fe4a1fb31eeb
CrystallineWatchmaker <antoineelliot.oury@gmail.com>
2024-01-27 21:35:32 +0000

    3d test

---
+++ threed.js
@@ -0,0 +1,5 @@
+﻿import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/8GOoemZckGh7wfsy/scene.splinecode');
